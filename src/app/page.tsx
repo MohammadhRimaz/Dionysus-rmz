abec1330488c335fa8263ae4f0bf435fd8094b9c
MohammadhRimaz <mohammadhrimaz@gmail.com>
2025-04-10 16:49:57 +0530

    Wrap button in a Link component to navigate to the dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Button>Click me</Button>
+      <Link href="/dashboard">
+        <Button>Click me</Button>
+      </Link>
     </div>
   );
 }
